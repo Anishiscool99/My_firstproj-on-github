205763a9c0c33a06dc07cb1ad133bb1a8bf10097
Anish Sharma <anish21.sharmakgp@gmail.com>
2023-09-16 20:07:18 +0530

    my first proj on github

---
+++ src/main/java/com/anishit/repo/UserRepo.java
@@ -0,0 +1,9 @@
+package com.anishit.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.anishit.binding.User;
+
+public interface UserRepo extends JpaRepository<User, Integer>{
+
+}
